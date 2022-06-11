3e448e5c22c962a85ccce08b8a3f5d808dc3af34
Jesse0x <Jesse@Github>
2022-06-10 22:11:27 -0400

    testing for github account

--- play.py
+++ play.py
@@ -14,12 +14,9 @@ count = 0
 for x in range(width):
     for y in range(height):
         pixel = im.getpixel((x, y))
-        a = count % 345
-        b = count % 234
-        c = count % 234
-        count += 1
-        if random.randint(0, 1):
-            im.putpixel((x, y), (a, b, c))
-        else:
-            im.putpixel((y, x), (a, b, c))
+        a = random.randint(0, 255)
+        b = random.randint(0, 255)
+        c = random.randint(0, 255)
+        im.putpixel((x, y), (a, b, c))
+        
 im.save("play.png")
